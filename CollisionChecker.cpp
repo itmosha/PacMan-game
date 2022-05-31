e0130e25bfc6c470eac2f023a3d8a27f49627425
itmosha <itmosha0@gmail.com>
2022-05-31 21:29:31 +0300

    collisions added

---
+++ CollisionChecker.cpp
@@ -0,0 +1,5 @@
+//
+// Created by itmosha on 31.05.22.
+//
+
+#include "CollisionChecker.h"
---
+++ CollisionChecker.h
@@ -0,0 +1,14 @@
+//
+// Created by itmosha on 31.05.22.
+//
+
+#ifndef NEWPROJECT_COLLISIONCHECKER_H
+#define NEWPROJECT_COLLISIONCHECKER_H
+
+
+class CollisionChecker {
+
+};
+
+
+#endif //NEWPROJECT_COLLISIONCHECKER_H
